198695420fadedd3e53cf23451749087e82a83c6
Deepika-cloud-code <vaddadi.d@northeastern.edu>
2024-09-19 20:23:59 -0400

    first commit

---
+++ Assignment_1/src/model/Person_Profile.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dwaraka Deepika vadd
+ */
+public class Person_Profile {
+String First_Name;
+String Last_Name;
+String Email;
+String Phone;
+String DL;
+String DL_State;
+String SSN;
+String Address_Line_1;
+String Address_Line_2;
+String City;
+String State;
+String ZIP;
+String Telephone_Number;
+String Fax_Number;
+String dob; // mine
+String Nationality;
+String Gender;
+String Hobbies;
+String Social_media_link;
+String degree_level;
+
+    public String getFirst_Name() {
+        return First_Name;
+    }
+
+    public void setFirst_Name(String First_Name) {
+        this.First_Name = First_Name;
+    }
+
+    public String getLast_Name() {
+        return Last_Name;
+    }
+
+    public void setLast_Name(String Last_Name) {
+        this.Last_Name = Last_Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDL() {
+        return DL;
+    }
+
+    public void setDL(String DL) {
+        this.DL = DL;
+    }
+
+    public String getDL_State() {
+        return DL_State;
+    }
+
+    public void setDL_State(String DL_State) {
+        this.DL_State = DL_State;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getAddress_Line_1() {
+        return Address_Line_1;
+    }
+
+    public void setAddress_Line_1(String Address_Line_1) {
+        this.Address_Line_1 = Address_Line_1;
+    }
+
+    public String getAddress_Line_2() {
+        return Address_Line_2;
+    }
+
+    public void setAddress_Line_2(String Address_Line_2) {
+        this.Address_Line_2 = Address_Line_2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTelephone_Number() {
+        return Telephone_Number;
+    }
+
+    public void setTelephone_Number(String Telephone_Number) {
+        this.Telephone_Number = Telephone_Number;
+    }
+
+    public String getFax_Number() {
+        return Fax_Number;
+    }
+
+    public void setFax_Number(String Fax_Number) {
+        this.Fax_Number = Fax_Number;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String Nationality) {
+        this.Nationality = Nationality;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getHobbies() {
+        return Hobbies;
+    }
+
+    public void setHobbies(String Hobbies) {
+        this.Hobbies = Hobbies;
+    }
+
+    public String getSocial_media_link() {
+        return Social_media_link;
+    }
+
+    public void setSocial_media_link(String Social_media_link) {
+        this.Social_media_link = Social_media_link;
+    }
+
+    public String getDegree_level() {
+        return degree_level;
+    }
+
+    public void setDegree_level(String degree_level) {
+        this.degree_level = degree_level;
+    }
+
+}
